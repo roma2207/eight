f294fcfd750a8220aa569047dd81961c4c320d0b
roma2207 <roma.rukh@gmail.com>
2019-07-09 15:05:06 +0200

    added file

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace eight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some text");
+            string text;
+            text = Console.ReadLine();
+            string letter = "@";
+            int count = text.Where(a => a.ToString() == letter).ToList().Count();
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
